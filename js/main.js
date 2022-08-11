2fe3f7efddddb48602b0965a110b2b95dc03d06e
AzizZouaghia <azizzouaghia360@gmail.com>
2022-08-11 15:30:09 +0100

    New Update
    1.Update The Bootsrap Version
    2.Update And Fix The NavBar
    3.Add The FontAwesome JS File
    4.Add To TheTop Button
    5.Remove The Side White Space

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//To Top Starts
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";}}
+mybutton.onclick =()=>{
+    window.scrollTo(0,0);
+}
+//To Top Ends
